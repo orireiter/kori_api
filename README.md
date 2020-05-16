7aef288156c08a0b15f6e87f604bb63ae85738af
orireiter <60241083+orireiter@users.noreply.github.com>
2020-05-16 16:01:43 +0300

    add uploading files and creating folders functionality

---
+++ README.md
@@ -0,0 +1,6 @@
+made by Ori Reiter (backend) & Koren Hamra (frontend)
+
+uploaded files should be sent as an form-data with
+ key -> "file"
+ value -> the actual file
+ content type -> multipart/form-data
--- file_api.py
+++ file_api.py
@@ -3,7 +3,8 @@ from flask import Flask, request, jsonify, send_from_directory
 from flask_cors import CORS
 import configparser, os
 from os import walk
-
+from werkzeug.utils import secure_filename
+from datetime import datetime
 
 #------------------------------------------------------------------#
 # PARSER related
@@ -81,8 +82,8 @@ def get_dir(**kwargs):
     return jsonify(post_container)
 
 #-------------------------------------------------#
-# route to Download docs and folders in the shared folder
-@app.route("/docs_down/<path:path>", methods=["GET"])
+# route to Download docs in the shared folder
+@app.route("/docs_transfer/<path:path>", methods=["GET"])
 def download(path):
     # breaking down the path given to filename
     # and relative path if needed
@@ -102,7 +103,79 @@ def download(path):
     return send_from_directory(fullpath, filename=filename, as_attachment=True)
 
 #-------------------------------------------------#
+# route to Upload docs in the shared folder
+@app.route("/docs_transfer/", methods=["POST"])
+@app.route("/docs_transfer/<path:path>", methods=["POST"])
+def upload(**kwargs):    
+    
+    var1 =  list(kwargs.items())
+    try:
+        full_path = root1 + var1[0][1]
+    except:
+        full_path = root1
+    print(f"full_path: {full_path}")
 
+    # verifying directory exists
+    if os.path.exists(full_path) == False:
+        return "no such directory", 404
+    else:
+        new_file = request.files["file"]
+        # verifying a file was really sent
+        if new_file.filename == "":
+            return "no file sent", 404
+        else:
+            dir_n_file = full_path+"/"+new_file.filename
+            print(f"dir_n_file: {dir_n_file}")
+
+            # checking there's no file with this name 
+            # and modifying it if necessary - adding date+time
+            if os.path.exists(dir_n_file):
+                now = str(datetime.now())
+                nowlist = now.split(".")
+                nowlist.pop(-1)
+                now = nowlist[0]
+                print(now)
+                
+                new_file.filename = new_file.filename + now
+                print(new_file.filename)
+
+            # securing the file name so it won't be used
+            # to plant the file anywhere
+            filename = secure_filename(new_file.filename)
+            print(f"secure filename: {filename}")
+            # saving it
+            new_file.save(os.path.join(full_path, filename))
+
+    return "success uploading"
+
+
+#-------------------------------------------------#
+# route to Create FOLDERS in the shared folder
+@app.route("/docs_mkdir/<path:path>", methods=["POST"])
+def mk_dir(path):
+    try:
+        extra_path = path.split("/")
+        new_folder = extra_path[-1]
+        extra_path.pop(-1)
+        fullpath = root1
+    
+        for extra in extra_path:
+            fullpath = fullpath + extra + "/"    
+    except:
+        fullpath = root1
+        new_folder = path
+    print(f"full path: {fullpath}")
+    
+    # checking if the path exist already with new folder
+    if os.path.exists(fullpath+"/"+new_folder):
+        return "error folder exists", 404
+    else:
+        # checking all the path is real 
+        if os.path.exists(fullpath) == False:
+            return "full path's mid folders dont exist", 404
+        else:
+            os.mkdir(fullpath+"/"+new_folder)
+            return "created new folder"
 
 
 #-------------------------------------------------#
